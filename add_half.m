20796daddb60e508615668826be16b5092112301
Nathan Davey <sprockmonty@gmail.com>
2019-12-07 16:14:15 +0000

    Added current guide and images

---
+++ add_half.m
@@ -0,0 +1,3 @@
+function [add_half_result] = add_half( first_num, second_num) % this is called a function definition, becasue we define what the function does
+    add_half_result = first_num + 0.5*second_num;
+end
